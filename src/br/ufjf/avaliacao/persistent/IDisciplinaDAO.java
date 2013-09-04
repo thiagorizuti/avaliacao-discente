d8af3e678fe1126ff4a425c5b50b1cb1c65d734e
Thiago Rizuti <thiago.rizuti@ice.ufjf.br>
2013-09-04 16:12:18 -0300

    Novos métodos no DisciplinaDAO

--- src/br/ufjf/avaliacao/persistent/IDisciplinaDAO.java
+++ src/br/ufjf/avaliacao/persistent/IDisciplinaDAO.java
@@ -1,5 +1,13 @@
 package br.ufjf.avaliacao.persistent;
 
+import org.hibernate.HibernateException;
+
+import br.ufjf.avaliacao.model.Disciplina;
+
 public interface IDisciplinaDAO {
+
+
+	Disciplina retornaDisciplina(String nomeDisciplina)
+			throws HibernateException, Exception;
 
 }
--- src/br/ufjf/avaliacao/persistent/impl/DisciplinaDAO.java
+++ src/br/ufjf/avaliacao/persistent/impl/DisciplinaDAO.java
@@ -1,8 +1,32 @@
 package br.ufjf.avaliacao.persistent.impl;
 
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.criterion.Restrictions;
+
+import br.ufjf.avaliacao.model.Disciplina;
 import br.ufjf.avaliacao.persistent.GenericoDAO;
 import br.ufjf.avaliacao.persistent.IDisciplinaDAO;
 
 public class DisciplinaDAO extends GenericoDAO implements IDisciplinaDAO{
+	
+	@SuppressWarnings("finally")
+	@Override
+	public Disciplina retornaDisciplina(String nomeDisciplina) throws HibernateException, Exception {
+		Disciplina disciplina = null;
+		try {
 
+			Criteria criteria = getSession()
+					.createCriteria(Disciplina.class, "disciplina")
+					.add(Restrictions.eq("disciplina.nomeDisciplina", nomeDisciplina))
+					.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+
+			disciplina = (Disciplina) criteria.uniqueResult();
+		} catch (HibernateException e) {
+			System.err.println(e.fillInStackTrace());
+		} finally {
+			getSession().close();
+			return disciplina;
+		}
+	}
 }
