fb4d548ad155c3d337416f3fb72c9ba39bc94231
pacbittencourt <pacbittencourt@ice.ufjf.br>
2013-10-08 12:37:30 -0300

    Atualizados métodos do UsuarioDAO utilizando HQL

--- src/br/ufjf/avaliacao/persistent/IUsuarioDAO.java
+++ src/br/ufjf/avaliacao/persistent/IUsuarioDAO.java
@@ -1,14 +1,18 @@
 package br.ufjf.avaliacao.persistent;
 
+import java.util.List;
+
 import org.hibernate.HibernateException;
 
 import br.ufjf.avaliacao.model.Usuario;
 
 
 public interface IUsuarioDAO {
-	public Usuario retornaUsuario (String email, String senha) throws HibernateException, Exception;
-
-	public Usuario retornaUsuario(String nome) throws HibernateException, Exception;
-
 	Usuario retornaUsuarioEmail(String email) throws HibernateException, Exception;
+
+	public Usuario retornaUsuario(String email, String senha);
+
+	public Usuario retornaUsuario(String nome);
+
+	public List<Usuario> retornaProfessores();
 }
--- src/br/ufjf/avaliacao/persistent/impl/UsuarioDAO.java
+++ src/br/ufjf/avaliacao/persistent/impl/UsuarioDAO.java
@@ -1,19 +1,34 @@
 package br.ufjf.avaliacao.persistent.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
-import org.hibernate.criterion.Restrictions;
-
+import org.hibernate.Query;
 import br.ufjf.avaliacao.model.Usuario;
 import br.ufjf.avaliacao.persistent.GenericoDAO;
 import br.ufjf.avaliacao.persistent.IUsuarioDAO;
 
 public class UsuarioDAO extends GenericoDAO implements IUsuarioDAO {
 	
-	@SuppressWarnings("finally")
+	@Override
+	public Usuario retornaUsuario(String email, String senha) {
+		try {
+			Query query = getSession().createQuery("SELECT u FROM Usuario AS u LEFT JOIN FETCH u.curso WHERE u.email = :email AND u.senha = :senha");
+			query.setParameter("email", email);
+			query.setParameter("senha", senha);
+			
+			Usuario usuario = (Usuario) query.uniqueResult();
+	
+			getSession().close();
+			
+			if(usuario!=null)
+				return usuario;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/*@SuppressWarnings("finally")
 	@Override
 	public Usuario retornaUsuario(String email, String senha) throws HibernateException, Exception {
 		Usuario usuario = null;
@@ -32,9 +47,26 @@ public class UsuarioDAO extends GenericoDAO implements IUsuarioDAO {
 			getSession().close();
 			return usuario;
 		}
+	}*/
+	
+	@Override
+	public Usuario retornaUsuario(String nome) {
+		try {
+			Query query = getSession().createQuery("SELECT u FROM Usuario AS u LEFT JOIN FETCH u.curso WHERE u.nome = :nome");
+			query.setParameter("nome", nome);
+			
+			Usuario usuario = (Usuario) query.uniqueResult();
+			getSession().close();
+			
+			if (usuario!=null)
+				return usuario;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
-	@SuppressWarnings("finally")
+	/*@SuppressWarnings("finally")
 	@Override
 	public Usuario retornaUsuario(String nome) throws HibernateException, Exception {
 		Usuario usuario = null;
@@ -52,8 +84,27 @@ public class UsuarioDAO extends GenericoDAO implements IUsuarioDAO {
 			getSession().close();
 			return usuario;
 		}
+	}*/
+	
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<Usuario> retornaProfessores() {
+		try {
+			Query query = getSession().createQuery("SELECT u FROM Usuario AS u LEFT JOIN FETCH u.curso WHERE u.tipoUsuario = :tipoUsuario");
+			query.setParameter("tipoUsuario", 1);
+			
+			List<Usuario> professores = query.list();
+			getSession().close();
+			
+			if (professores!=null)
+				return professores;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
+	/*
 	public List<Usuario> retornaProfessores() {
 		@SuppressWarnings("unchecked")
 		List <Usuario> usuarios = (List<Usuario>) procuraTodos(Usuario.class, -1, -1);
@@ -64,8 +115,24 @@ public class UsuarioDAO extends GenericoDAO implements IUsuarioDAO {
 		}  
 		return professores;
 	}
+	*/
 	
-	@SuppressWarnings("finally")
+	public Usuario retornaUsuarioEmail(String email) {
+		try {
+			Query query = getSession().createQuery("SELECT u FROM Usuario AS u LEFT JOIN FETCH u.curso WHERE u.email = :email");
+			query.setParameter("email", email);
+			
+			Usuario usuario = (Usuario) query.uniqueResult();
+			getSession().close();
+			if (usuario!=null)
+				return usuario;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/*@SuppressWarnings("finally")
 	@Override
 	public Usuario retornaUsuarioEmail(String email) throws HibernateException, Exception {
 		Usuario usuario = null;
@@ -83,6 +150,6 @@ public class UsuarioDAO extends GenericoDAO implements IUsuarioDAO {
 			getSession().close();
 			return usuario;
 		}
-	}
+	}*/
 
 }
